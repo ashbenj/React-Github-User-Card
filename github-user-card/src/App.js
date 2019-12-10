8d4d8f06422a64554e50a684ec2a55a55fd46e68
Asha <ashalbenjamin@gmail.com>
2019-12-10 00:15:36 -0600

    information-for-users-rendering-still-need-tweaking

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from 'axios';
+import '../src/components/App.css';
 
-class App extends Component {
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			user: 'ashbenj',
+			followers: []
+		};
+	}
+
+	componentDidMount() {
+		this.getUsers();
+		this.getFollowers();
+	}
+
+	getUsers = () => {
+		axios
+			.get(`https://api.github.com/users/${this.state.user}`)
+			.then(response => {
+				console.log('data from user axios', response.data);
+				this.setState({ user: response.data });
+			})
+			.catch(error => {
+				console.log('Data was not returned', error);
+			});
+	};
+
+	getFollowers = () => {
+		axios
+			.get(`https://api.github.com/users/${this.state.user}/following`)
+			.then(response => {
+				console.log('data from followers axios', response.data);
+				this.setState({ followers: response.data });
+			})
+			.catch(error => {
+				console.log('Data was not returned', error);
+			});
+	};
+
 	render() {
 		return (
-			<>
-				<h1>Github Users</h1>
-			</>
+			<div>
+				<h1>Github User Card</h1>
+				<div className='user-content'>
+					<h3 className='user-name'>{this.state.user.name}</h3>
+					<img
+						className='user-img'
+						src={this.state.user.avatar_url}
+						alt="user's avatar"
+					/>
+					<div>
+						<p>Bio:{this.state.user.bio}</p>
+						<p>Location:{this.state.user.location}</p>
+						<p>
+							Followers:{this.state.followers.map(follower => follower.login)}
+						</p>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
